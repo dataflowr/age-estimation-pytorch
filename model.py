a4ce2aa72b011e9fa3992a45c2190c29da96e39b
faroukhlifi <43809863+faroukhlifi@users.noreply.github.com>
2021-03-09 16:08:02 +0100

    Update model.py

--- model.py
+++ model.py
@@ -8,18 +8,44 @@ class Homosedastic(nn.Module):
         super(Homosedastic, self).__init__()
 
         self.common =  pretrainedmodels.__dict__[model_name](pretrained=pretrained)
-        print("original model",model)
+        #print("original model",model)
         dim_feats = model.last_linear.in_features
         #The homosedastic model outputs 1 element (the age)
-        model.last_linear = nn.Sequential(nn.Linear(dim_feats,101),nn.Linear(101, 40) ,nn.Linear(40,1), nn.ReLU())
+        self.common.last_linear = nn.Sequential(nn.Linear(dim_feats,101),nn.Linear(101, 40) ,nn.Linear(40,1), nn.ReLU())
         #model.avg_pool = nn.Identity()
-        print("new model",model)
+        #print("new model",model)
         
         self.hom_error = nn.Parameter(torch.zeros(1).float())
 
     def forward(self, x):
+        
         return self.common(x)
+
+class CORAL(nn.Module):   
     
+    def __init__(self, model_name='vgg13', num_classes = 101):
+        super(Homosedastic, self).__init__()
+
+        self.common =  pretrainedmodels.__dict__[model_name](pretrained=pretrained)
+        #print("original model",model)
+        dim_feats = model.last_linear.in_features
+        #The last layer is common to all binary decisions and is bias_free
+        self.common.last_linear = nn.Linear(dim_feats, num_classes, bias= False)
+        #model.avg_pool = nn.Identity()
+        #print("new model",model)
+        
+        self.biases = nn.Parameter(torch.zeros(num_classes -1).float())
+      
+    def forward(self, x):
+        
+        x = self.common(x)
+        logits = logits + self.biases
+        probas = torch.sigmoid(logits)
+        
+        return logits, probas
+    
+        
+        
 def get_model(model_name="vgg13", num_classes=101, pretrained="imagenet",homosedastic=False):
     
     if not homosedastic:
